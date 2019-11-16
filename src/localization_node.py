ac9b4e814b61231219d89a36c67039ecd9780207
Johannes Boghaert <johannes@MacBook-Pro-van-Johannes.local>
2019-11-16 13:02:00 +0100

    Updated graph and cleaner code .. waiting for state-estimation & testing

--- src/localization_node.py
+++ src/localization_node.py
@@ -2,28 +2,187 @@
 # Search for TODO, or correct to see issues to be resolved
 
 
-# TITLE
-# TRIGGER FUNCTION FOR LOCALIZATION AFTER FIRST AT DETECTION
-# TODO: LOOK into node IND_NAV OF COORDINATION OR NAVIGATION FOR (DETECT RED LINE & AT DETECTION), THEN THERE IS A SWITCH TO TELL DB TO GO R, L OR STRAIGHT
+# TITLE: TRIGGER FUNCTION TO START LOCALIZATION? PATH PLANNING AND PUBLISH WHEEL CMD
 
-# TODO: if first apriltag is detected, go to path planning, if second AT is detected, go to other part of the code where wheel commands based on the generated path are published to wheel_cmd_node
-# def pathProcessor(self, path): generates the wheelcommands from the path (wheelCmdList as output array)
-# def cmdToWheel(self, AT_msg, wheelCmdList): publishes the relevant wheelcmd in runtime. Use pop out function to update the wheelcmd that needs to be published
-# Stop localization node once 1st AT has come in
-
-# WARNING: TODO the actual execution should also take into account the traffic rules/lane directions - use another processing node to assign task priority
-
-
-# DESCRIPTION
+# DESCRIPTION:
 # This script takes input from apriltags_postprocessing_node and checks if a localizable AT (that is included in our predefined map) is reached
 # If so, it produces a usable input to the path_planning module for further processing and therefore acts as a switch, if not, AT_detection and lane_following continue
+# The path_planning module returns the shortest path and executable wheel commands that are then published to unicorn_intersection_node
 
+# ----------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+
+# IMPORT
+import numpy as np
+import os
+import rospy
+import yaml
+
+from duckietown import DTROS
+from duckietown_msgs.msg import AprilTagsWithInfos, TagInfo, BoolStamped, AprilTagDetection, TurnIDandType
+
+from path_planning_class import PathPlanner
+
+
+# INITIATE DTROS CLASS (incl sub/pub)
+class LocalizationNode(DTROS):
+
+    def __init__(self, node_name):
+        # Initialize, specify 'node_name' further in 'if __name__ ...'
+        super(LocalizationNode, self).__init__(node_name=localization_node)
+
+        # Initialize variables
+        self.node_name = rospy.get_name()
+        self.log = rospy.loginfo() #correct?
+        rospy.loginfo("[%s] Initializing." % (self.node_name))
+        self.AT = False
+
+        # Get arrival point (from roslaunch/docker cmd setting the parameter 'goal_input' in terminal)
+        self.input = rospy.get_param('~goal_input') # of type [id, distance before final AT]
+        rospy.set_param('~goal_input', self.goal) # Write to parameter server for transparancy
+        self.goal = self.input[0]
+        self.goal_distance = self.input[1]
+
+        # Import from external class PathPlanner as pp
+        self.pp = PathPlanner()
+        self.tags = self.pp.tags
+        self.graph = self.pp.graph
+
+        # List subscribers
+        self.sub_AT_detection = rospy.Subscriber('~apriltags_out', AprilTagsWithInfos, self.callback) #from apriltags_postprocessing_node
+
+        # List publishers
+        self.pub_direction_cmd = rospy.Publisher('~turn_id_and_type', TurnIDandType, queue_size = 1, latch = True) # to unicorn_intersection_node
+
+        # Conclude
+        rospy.loginfo("[%s] Initialized." % (self.node_name))
+        self.rate = rospy.Rate(10)
+
+
+# CODE GOES HERE
+
+    def callback(self, msg):
+        #correct? Should not matter - ask to implement clean code ~with FSM
+        if self.fsm_mode == "INTERSECTION_CONTROL" or self.fsm_mode == "INTERSECTION_COORDINATION" or self.fsm_mode == "INTERSECTION_PLANNING":
+
+            # Define path and cmd list from localized, current point in time
+            trigger, starting_point = self.localization(msg)
+            path, cmd = self.path_planning(trigger, starting_point)
+
+            # If there is an actual path to be executed, run the following (path-actuation):
+            if len(path) > 2:
+                # from generated SP, generate sequence of wheel command of type TurnIDandType to publish
+                wheelcommand = self.pathProcessor(path, cmd)
+                self.pub_direction_cmd.publish(wheelcommand)
+
+            # After the turn cmd of the before-last AT, start state-estimation (due to definition arrival point B):
+            elif len(path) == 2:
+                # Import from another class
+                print("start state estimation")
+
+            # If the final AT is detected (could happen when arrival point B is too close to final AT), ignore input:
+            else:
+                pass #correct? is this necessary? Is there a state-switch? Can we not send any command at all to the publish-topic?
+
+
+    def localization(self, msg):
+        # Process incoming msg from ~apriltags_out of type AprilTagsWithInfos
+
+        for item in msg.detections:
+            # WARNING: make sure only one id in Tags is detected, else vector size is > 1 --> account for in DT set up
+            # If AT id is part of self.tags, continue with localization, else pass
+            if item.id in self.tags:
+                # Detect localizable AT
+                self.AT = True
+                self.log('AT successfully detected, localized duckiebot in map ... proceeding to path planning')
+
+                # Return usable input for path_planning (graph is based on string variables)
+                starting_point = str(item.id)
+                return self.AT, starting_point
+
+            # IGNORE: current map assumes only readible AT's - use as set-up for future project
+            else:
+                # WARNING: This needs to be updated if e.g. other apriltags are implemented in DT, and another command needs to be executed. Keep lane_following and AT_detection alive
+                # Idea is to have another node subscribe to this topic, and return an action if AT is NOT part of self.tags, and don't do anything if otherwise
+                self.AT = False
+                starting_point = "No readible AT encountered"
+                self.log(starting_point)
+                return self.AT, starting_point #correct? or should this be pass/break?
+
+
+    def path_planning(self, AT, starting_point):
+        # Calculate shortest path from current AT in time
+
+        if AT == False:
+            print(starting_point)
+            pass #correct? Keep lane_following and AT_detection alive
+
+        else:
+            self.log('Starting path planning')
+            print("... starting path planning")
+            # Define start and end point (dynamic)
+            start = starting_point
+            goal = self.goal
+
+            # Run path planning from imported class (defined in initializer)
+            path, cmd = self.pp.dijkstra(start, goal)
+            rospy.loginfo('Confirmation: Path successfully generated !')
+            return path, cmd
+
+
+    def pathProcessor(self, path, cmd):
+        # Process the incoming path and cmd lists and return image for publisher
+        # WARNING: the path is updated every time an AT is detected - allows for dynamic changes in the map (when not static) or a new end goal, but adds to the computation time
+
+        # Take current node and command
+        cmd_comb = [path[0], cmd[0]]
+        # Set up outgoing message type
+        new_cmd = TurnIDandType()
+        new_cmd.tag_id = cmd_comb[0]
+
+        # Define the turn command
+        # WARNING: unnecessary code, but allows more flexibility for future projects (different indices)
+        # The index 0, 1 and 2 is used for turn_type msg type (see also random_april_tag_turns_node and open_loop_intersection_node)
+        if cmd_comb[1] == 0:
+            new_cmd.turn_type = 0
+            self.log("Turn left")
+        elif cmd_comb[1] == 1:
+            new_cmd.turn_type = 1
+            self.log("Go straight")
+        elif cmd_comb[1] == 2:
+            new_cmd.turn_type = 2
+            self.log("Turn right")
+
+        return new_cmd
+
+# SAFETY & EMERGENCY
+    def on_shutdown(self):
+        rospy.loginfo("[%s] Shutting down." % (self.node_name)) #correct?
+
+
+# KEEP NODE ALIVE
+if __name__ == "__main__":
+    # Initialize the node with rospy
+    localization_node = LocalizationNode(node_name="localization_node")
+    # Allow proper shutdown
+    node = LocalizationNode()
+    rospy.on_shutdown(node.on_shutdown)
+    # Keep it spinning to keep the node alive
+    rospy.spin()
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+# TO BE DONE:
+# TODO: LOOK into node IND_NAV OF COORDINATION OR NAVIGATION FOR (DETECT RED LINE & AT DETECTION), THEN THERE IS A SWITCH TO TELL DB TO GO R, L OR STRAIGHT
+# def cmdToWheel(self, AT_msg, wheelCmdList): publishes the relevant wheelcmd in runtime. Use pop out function to update the wheelcmd to be published # Stop localization node once 1st AT has come in?
+# TODO: the actual execution should also take into account the traffic rules/lane directions - use another processing node to assign task priority
+# TODO: # Look into coordinator node, unicorn intersection and random AT # rospy.loginfo, pop out function python
 
 # OVERALL SOLUTION/PIPELINE
 # First part of GOTO-1 solution:
 # Activate indefinite_navigation (https://github.com/duckietown/dt-core/tree/daffy/packages/indefinite_navigation) OR lane_following () in parallel to AT_detection () and this node.
 # When AT detected, localize in predefined map (according to DT protocol), and continue to path_planning
-
 
 # QUESTIONS
 # Question_1: where to implement? Implement in AT_detection_node and then send message to stop lane_following and run localization_node?
@@ -49,169 +208,4 @@
 
 # Question_10: is the parameter self.goal updated everytime it is called for? s.t. during the deployment of the goto-1 solution,
 # the end goal can be updated from the command line? Guess yes.
-
-# ------------------------------------------------------------------------------------------------------------------------------------ #
-
-# IMPORT
-import numpy as np
-import os
-import rospy
-import yaml
-
-from duckietown import DTROS
-from duckietown_msgs.msg import AprilTagsWithInfos, TagInfo, BoolStamped, AprilTagDetection, TurnIDandType
-
-from path_planning_class import PathPlanner
-
-
-# INITIATE DTROS CLASS (incl sub/pub)
-class LocalizationNode(DTROS):
-
-    def __init__(self, node_name):
-        #Initialize, specify 'node_name' further in 'if __name__ ...'
-        super(LocalizationNode, self).__init__(node_name=localization_node)
-
-        # Initialize variables
-        self.node_name = rospy.get_name()
-        self.log = rospy.loginfo() #correct?
-        rospy.loginfo("[%s] Initializing." % (self.node_name))
-        self.AT = False
-
-        # Get arrival point (from roslaunch/docker cmd setting the parameter 'goal_input' in terminal)
-        self.goal = rospy.get_param('~goal_input')
-        rospy.set_param('~goal_input', self.goal) # Write to parameter server for transparancy
-
-        # List all AT id's (int32[] type) that are considered for localization purposes in the predefined map
-        # Import from external class PathPlanner as pp
-        self.pp = PathPlanner()
-        self.tags = self.pp.tags
-        self.graph = self.pp.graph
-
-        # List subscribers
-        self.sub_AT_detection = rospy.Subscriber('~apriltags_out', AprilTagsWithInfos, self.callback) #from apriltags_postprocessing_node
-
-        # List publishers
-        #self.pub_override = rospy.Publisher("~/tag_detections", BoolStamped, queue_size = 1)
-        #self.pub_pause = rospy.Publisher("~send_msg_to_lane_following_node", BoolStamped, queue_size = 1)
-        self.pub_direction_cmd = rospy.Publisher('~turn_id_and_type', TurnIDandType, queue_size = 1, latch = True) # to unicorn_intersection_node
-
-        # Conclude
-        rospy.loginfo("[%s] Initialized." % (self.node_name))
-        self.rate = rospy.Rate(10)
-
-
-# CODE GOES HERE
-# Look into coordinator node, unicorn intersection and random AT
-# rospy.loginfo, pop out function python
-
-    def callback(self, msg):
-        #correct? Should not matter - ask to implement clean code ~with FSM
-        if self.fsm_mode == "INTERSECTION_CONTROL" or self.fsm_mode == "INTERSECTION_COORDINATION" or self.fsm_mode == "INTERSECTION_PLANNING":
-            # Toplevel function that is invoked by subscriber and then publishes commands to relevant topic
-            # Define path from localized starting point/or current point in time
-            path = self.path_planning(self.localization(msg))
-
-            # If there is an actual path to be executed, run the following (path-actuation):
-            if len(path) > 1:
-                # from generated SP, generate sequence of wheel command of type TurnIDandType to publish
-                wheelcommand = self.pathProcessor(path)
-
-                # publish relevant wheel command of type TurnIDandType
-                self.pub_direction_cmd.publish(wheelcommand)
-
-            # If there is no path, and the last AT is encountered, run the following (state-estimation):
-            else:
-                "Do something"
-
-            #IGNORE: find when a single command should be published (and for how long)
-            #IGNORE: this is not sufficient to allow enough time for wheelcmd execution (rospy.sleep(5.0))
-
-
-    def localization(self, msg):
-        # Process incoming msg from ~apriltags_out of type AprilTagsWithInfos
-        for item in msg.detections:
-            # WARNING: make sure only one id in Tags is detected, else vector size is > 1 --> account for in DT set up
-            # If AT id is part of self.tags, continue with localization, else pass
-            if item.id in self.tags:
-                # Detect localizable AT
-                self.AT = True
-                self.log('AT successfully detected, localized duckiebot in map')
-                print("AT successfully detected, localized duckiebot in map ... now proceeding to path planning")
-
-                # Return usable input for path_planning (graph is based on string variables)
-                starting_point = str(item.id)
-                return self.AT, starting_point
-
-            else:
-                # This needs to be updated if e.g. other apriltags are implemented in DT, and another command needs to be executed.
-                # Idea is to have another node subscribe to this topic, and return an action if AT is NOT part of self.tags, and don't do anything if otherwise
-                # Keep lane_following and AT_detection alive
-                self.AT
-                starting_point = "No readible AT encountered"
-                self.log('No readible AT encountered')
-                return self.AT, starting_point #correct? or should this be pass/break?
-
-
-    def path_planning(self, AT, starting_point):
-        # Run localization
-        # AT, starting_point = self.localization(msg)
-        # Only proceed if localized
-        if AT == False:
-            # Keep lane_following and AT_detection alive
-            print(starting_point)
-            pass #correct?
-
-        else:
-            self.log('Starting path planning')
-            print("... starting path planning")
-            # Copy starting point found in localization
-            start = starting_point
-            # Define arrival point from manual input in docker run cmd/terminal
-            goal = self.goal
-            # Define map (in terms of weights)
-            graph = self.graph
-
-            # Run path planning from imported class (defined in initializer)
-            path = self.pp.dijkstra(start, goal)
-            rospy.loginfo('Path successfully generated')
-            print("Confirmation: path successfully generated !")
-            return path
-
-
-    def pathProcessor(self, path):
-        # WARNING: the path is updated every time an AT is detected - allows for dynamic changes in the map (once this is not made static) or a new end goal, but adds to the computation time
-        # Take second entry (cmd) from the first list from the newly iterated path
-        cmd = path[0]
-        # Set up outgoing message type
-        new_cmd = TurnIDandType()
-        new_cmd.tag_id = cmd[0]
-        # Define the turn command
-        # WARNING: could be hardcoded in the map as well, but current set up allows more flexibility
-        if cmd[1] == 0:
-            # The index 0, 1 and 2 is used for turn_type msg type (see also random_april_tag_turns_node and open_loop_intersection_node)
-            new_cmd.turn_type = 0
-            self.log("Turn left")
-        elif cmd[1] == 1:
-            new_cmd.turn_type = 1
-            self.log("Go straight")
-        elif cmd[1] == 2:
-            new_cmd.turn_type = 2
-            self.log("Turn right")
-
-        return new_cmd
-
-# SAFETY & EMERGENCY
-    def on_shutdown(self):
-        rospy.loginfo("[%s] Shutting down." % (self.node_name))
-
-
-# KEEP NODE ALIVE
-if __name__ == "__main__":
-    # Initialize the node with rospy
-    localization_node = LocalizationNode(node_name="localization_node")
-    # Allow proper shutdown
-    node = LocalizationNode()
-    rospy.on_shutdown(node.on_shutdown)
-    # Keep it spinning to keep the node alive
-    rospy.spin()
-
+# Question_11: General functioning of else pass/break/continue statements. What is correct?
--- src/path_planning_class.py
+++ src/path_planning_class.py
@@ -37,55 +37,100 @@ class PathPlanner:
         # Define weights/costs between AT's to base SSP on - this is the predefined DT map (manual input)
         # Each intersection has as many different AT's as it has roads (e.g. a T-intersection has 3 roads, and for each road another AT id)
         # TODO: import as .yaml file from external source (have a map for each DT configuration, or configure from watchtowers)
-        graph = {'a1': {'b1': 4.7, 'c1': 2.7}, 
-                'a2': {'c1': 1.9, 'c1': 6.9}, 
-                'a3': {'b1': 5.4, 'c1': 7.6},
-        
-                'b1': {'c2': 1.9, 'f1': 2}, 
-                'b2': {'a2': 3.9, 'c2': 1.9}, 
-                'b3': {'a2': 3.8, 'f1': 2.7},
-    
-                'c1': {'b3': 1.9,  'd1': 2.7,  'g1':2}, 
-                'c2': {'a3': 2.7,  'd1': 2,    'g1':1.9},
-                'c3': {'a3': 2,    'b3': 2.7,  'd1':1.9},
-                'c4': {'a3': 1.9,  'b3': 2,    'd1':2.7}, 
+        self.graph = {
+                    'a1': {'b1': 4.7, 'c1': 2.7}, 
+                    'a2': {'c1': 1.9, 'c1': 6.9}, 
+                    'a3': {'b1': 5.4, 'c1': 7.6},
+
+                    'b1': {'c2': 1.9, 'f1': 2}, 
+                    'b2': {'a2': 3.9, 'c2': 1.9}, 
+                    'b3': {'a2': 3.8, 'f1': 2.7},
+
+                    'c1': {'b3': 1.9,  'd1': 2.7,  'g1':2}, 
+                    'c2': {'a3': 2.7,  'd1': 2,    'g1':1.9},
+                    'c3': {'a3': 2,    'b3': 2.7,  'd1':1.9},
+                    'c4': {'a3': 1.9,  'b3': 2,    'd1':2.7}, 
 ​
-                'd1': {'e1': 3, 'h1': 1.9}, 
-                'd2': {'c4': 2.7, 'e1': 2.9},
-                'd3': {'c4': 2, 'h1': 2.7},
-         
-                'e1': {'a1': 9.4, 'j3': 6.8}, 
-                'e2': {'d3': 2.9, 'j3': 6.9},
-                'e3': {'a1': 7.7, 'd3': 3.7},
+                    'd1': {'e1': 3, 'h1': 1.9}, 
+                    'd2': {'c4': 2.7, 'e1': 2.9},
+                    'd3': {'c4': 2, 'h1': 2.7},
+
+                    'e1': {'a1': 9.4, 'j3': 6.8}, 
+                    'e2': {'d3': 2.9, 'j3': 6.9},
+                    'e3': {'a1': 7.7, 'd3': 3.7},
 ​
-                'f1': {'g2': 2.7, 'i2': 4.7}, 
-                'f2': {'b2': 2,   'g2': 1.9},
-                'f3': {'b2': 1.9, 'i2': 5.4},
+                    'f1': {'g2': 2.7, 'i2': 4.7}, 
+                    'f2': {'b2': 2,   'g2': 1.9},
+                    'f3': {'b2': 1.9, 'i2': 5.4},
 ​
-                'g1': {'f3': 1.9,  'h2': 2.7,  'i1':2},
-                'g2': {'c3': 2.7,  'h2': 2,    'i1':1.9},
-                'g3': {'c3': 2,    'f3': 2.7,  'h2':1.9},
-                'g4': {'c3': 1.9,  'f3': 2,    'i1':2.7},
+                    'g1': {'f3': 1.9,  'h2': 2.7,  'i1':2},
+                    'g2': {'c3': 2.7,  'h2': 2,    'i1':1.9},
+                    'g3': {'c3': 2,    'f3': 2.7,  'h2':1.9},
+                    'g4': {'c3': 1.9,  'f3': 2,    'i1':2.7},
 ​
-                'h1': {'g4': 1.9, 'j1': 2},
-                'h2': {'d2': 2.7, 'j1': 1.9},
-                'h3': {'g4': 2.7, 'd2': 2},
+                    'h1': {'g4': 1.9, 'j1': 2},
+                    'h2': {'d2': 2.7, 'j1': 1.9},
+                    'h3': {'g4': 2.7, 'd2': 2},
 ​
-                'i1': {'f2': 3.8, 'j2': 2.7},
-                'i2': {'g3': 2.7, 'j2': 2},
-                'i3': {'f2': 3.9, 'g3': 1.9},
+                    'i1': {'f2': 3.8, 'j2': 2.7},
+                    'i2': {'g3': 2.7, 'j2': 2},
+                    'i3': {'f2': 3.9, 'g3': 1.9},
 ​
-                'j1': {'e3': 8.4, 'i3': 1.9},
-                'j2': {'e3': 7.7, 'h3': 2.7},
-                'j3': {'h3': 1.9, 'i3': 2}}
+                    'j1': {'e3': 8.4, 'i3': 1.9},
+                    'j2': {'e3': 7.7, 'h3': 2.7},
+                    'j3': {'h3': 1.9, 'i3': 2}}
 
         """
+        Use this version for the demo version (with AT implemented)
         self.graph = {iD[1]: {iD[2]: 10, iD[3]: 4},
                     iD[2]: {iD[4]: 8, iD[4]: 2},
                     iD[3]: {iD[2]: 6, iD[4]: 2, iD[5]: 2},
                     iD[4]: {iD[5]: 7},
                     iD[5]: {iD[4]: 9}}
         """
+
+        self.graph_direction = {
+                    'a1': {'b1': 1, 'c1': 0},
+                    'a2': {'c1': 2, 'e2': 1},
+                    'a3': {'b1': 0, 'e2': 2},
+
+                    'b1': {'c2': 0, 'f1': 1},
+                    'b2': {'a2': 1, 'c2': 2},
+                    'b3': {'a2': 2, 'f1': 0},
+
+                    'c1': {'b3': 2,  'd1': 0,  'g1':1},
+                    'c2': {'a3': 0,  'd1': 1,  'g1':2},
+                    'c3': {'a3': 1,  'b3': 0,  'd1':2},
+                    'c4': {'a3': 2,  'b3': 1,  'g1':0},
+
+                    'd1': {'e1': 1, 'h1': 2},
+                    'd2': {'c4': 0, 'e1': 2},
+                    'd3': {'c4': 1, 'h1': 0},
+
+                    'e1': {'a1': 0, 'j3': 2},
+                    'e2': {'d3': 2, 'j3': 1},
+                    'e3': {'a1': 1, 'd3': 0},
+
+                    'f1': {'g2': 0, 'i2': 1},
+                    'f2': {'b2': 1, 'g2': 2},
+                    'f3': {'b2': 2, 'i2': 0},
+
+                    'g1': {'f3': 2,  'h2': 0,  'i1':1},
+                    'g2': {'c3': 0,  'h2': 1,  'i1':2},
+                    'g3': {'c3': 1,  'f3': 0,  'h2':2},
+                    'g4': {'c3': 2,  'f3': 1,  'i1':0},
+
+                    'h1': {'g4': 2, 'j1': 1},
+                    'h2': {'d2': 0, 'j1': 2},
+                    'h3': {'g4': 0, 'd2': 1},
+
+                    'i1': {'f2': 2, 'j2': 0},
+                    'i2': {'g3': 0, 'j2': 1},
+                    'i3': {'f2': 1, 'g3': 2},
+
+                    'j1': {'e3': 0, 'i3': 2},
+                    'j2': {'e3': 1, 'h3': 0},
+                    'j3': {'h3': 2, 'i3': 1}}
 
 
     def dijkstra(self, start, goal):
@@ -94,6 +139,7 @@ class PathPlanner:
         predecessor = {}
         infinity = 9999999
         path = []
+        cmd = []
 
         for node in self.graph:
             shortest_distance[node] = infinity
@@ -111,22 +157,26 @@ class PathPlanner:
                 if weight + shortest_distance[minNode] < shortest_distance[childNode]:
                     shortest_distance[childNode] = weight + shortest_distance[minNode]
                     predecessor[childNode] = minNode
-            graph.pop(minNode)
+            self.graph.pop(minNode)
 
         currentNode = goal
         while currentNode != start:
             try:
                 path.insert(0, currentNode)
+                cmd.insert(0, self.graph_direction[predecessor[currentNode]][currentNode])
                 currentNode = predecessor[currentNode]
             except KeyError:
                 print('Path not reachable')
                 break
+
         path.insert(0, start)
         if shortest_distance[goal] != infinity:
             print('Shortest distance is ' + str(shortest_distance[goal]))
             print('And the path is ' + str(path))
+            print('And the sequence of wheel cmds is' + str(cmd))
             print('... waiting for confirmation from path_planning module ...')
 
-        return path
+        return path, cmd
+        # WARNING: len(path) = (len(cmd) + 1) !!
         # returns sequence of nodes in string format, this string will be the str-version of the AT id in int32 format
         # this should allow easy calculation/transformation back and forth
